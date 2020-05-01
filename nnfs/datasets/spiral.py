8edac3edbf65a89ab53e452f260153297c9765f4
Daniel Kukieła <daniel@kukiela.pl>
2020-05-01 13:33:53 +0200

    Changed back "t" factor to 0.2

--- nnfs/datasets/spiral.py
+++ nnfs/datasets/spiral.py
@@ -10,7 +10,7 @@ def create_data(points, classes):
     for class_number in range(classes):
         ix = range(points*class_number, points*(class_number+1))
         r = np.linspace(0.0, 1, points)
-        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.05
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
         X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
         y[ix] = class_number
     return X, y
